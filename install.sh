deaddad17f1a7e2f8e679c3e9958a465de33bac1
invakid404 <invakid404@riseup.net>
2021-09-12 23:55:30 +0300

    refactor install script

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-is_user_root () { [ "${EUID:-$(id -u)}" -eq 0 ]; }
+is_user_root() {
+	[ "${EUID:-$(id -u)}" -eq 0 ]
+}
 
-sh -c "cd home && stow -v -R -t $HOME *"
+install() {
+	source_dir=$1
+	target_dir=$2
+
+	sh -c "cd ${source_dir} && stow -v -R -t ${target_dir} *"
+}
+
+install home "${HOME}"
 
 if is_user_root; then
-	sh -c "cd root && stow -v -R -t / *"
+	install root /
 fi
