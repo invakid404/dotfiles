a67a6826ac0ebc3492215c731decf35ab498d119
invakid404 <invakid404@riseup.net>
2021-09-12 23:49:55 +0300

    add portage config files

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-cd home && stow -v -R -t $HOME *
+is_user_root () { [ "${EUID:-$(id -u)}" -eq 0 ]; }
+
+sh -c "cd home && stow -v -R -t $HOME *"
+
+if is_user_root; then
+	sh -c "cd root && stow -v -R -t / *"
+fi
