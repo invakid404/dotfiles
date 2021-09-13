deada3d86a114151d888dde8ed8be09589265f80
invakid404 <invakid404@riseup.net>
2021-09-13 23:13:11 +0300

    implement privilege escalation via various tools

--- install.sh
+++ install.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-is_user_root() {
-	[ "${EUID:-$(id -u)}" -eq 0 ]
+run_as_root() {
+	cmds="doas,sudo,su root -c"
+	IFS="," read -ra cmd_arr <<< "$cmds"
+
+	for cmd in "${cmd_arr[@]}"; do
+		cmd_name=$(echo "$cmd" | head -n1 | cut -d " " -f1)
+
+		if command -v "$cmd_name" &> /dev/null; then
+			echo "Using ${cmd_name} to escalate perms"
+
+			target="$cmd -- $@"
+			eval " $target"
+
+			return
+		fi
+	done
+
+	echo "Found nothing to escalate perms with, not installing root configs."
 }
 
 install() {
 	source_dir=$1
 	target_dir=$2
 
-	sh -c "cd ${source_dir} && stow -v -R -t ${target_dir} *"
+	echo "sh -c \"cd ${source_dir} && stow -v -R -t ${target_dir} *\""
 }
 
 git secret reveal -vfF || :
 
-install home "${HOME}"
+eval " $(install home '${HOME}')"
 
-if is_user_root; then
-	install root /
-fi
+run_as_root $(install root /)
